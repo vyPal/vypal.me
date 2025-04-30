9fd0449f667e76a7b39bf38a2496838f24c935c9
vyPal <kubik.palacky@gmail.com>
2025-04-30 06:38:34 +0200

    hydrateRoot instead of createRoot

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -11,9 +11,7 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
+        hydrateRoot(el, <App {...props} />);
     },
     progress: {
         color: '#4B5563',
